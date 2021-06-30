2622abe93c094bd9c486d9951f4daa2dd57e4505
GeorgeFunner <33177533+Danikalyk@users.noreply.github.com>
2021-06-30 19:00:03 +0700

    Easy Upgrade Module(for example for game)

---
+++ Prokachka/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Prokachka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Правопарядок:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Торговля:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Военное дело:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Очки: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Деньги: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(640, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 155);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Статистика";
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Prokachka.Properties.Resources.tribal_shield;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(423, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 136);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Prokachka.Properties.Resources.two_coins;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(222, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 136);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Prokachka.Properties.Resources.paranoia;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(12, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 136);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 380);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Prokachka/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prokachka
+{
+    public partial class Form1 : Form
+    {
+        //скиллы
+        private int war = 0;
+        private int buying = 0; 
+        private int goverment = 0;
+        //деньги
+        int money = 10000;
+        //очки для улучшения
+        int upgradeOchki = 5;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Prockachka(ref goverment);
+            spellLevel();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Prockachka(ref buying);
+            spellLevel();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Prockachka(ref war);
+            spellLevel();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            spellLevel();
+
+            label4.Text += upgradeOchki;
+            label5.Text = "Деньги: " + money;
+        }
+
+        public void spellLevel()
+        {
+            label1.Text = "Правопарядок: " + goverment;
+            label2.Text = "Торговля: " + buying;
+            label3.Text = "Военное дело: " + war;
+        }
+
+        public void Prockachka(ref int upgradeSpell)
+        {
+            if(upgradeOchki > 0)
+            {
+                upgradeOchki--;
+                upgradeSpell++;
+                label4.Text ="Очки: " + upgradeOchki;
+            }
+        }//улучшение способности
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (goverment == 1)
+                money = money + 50;
+
+            if (goverment == 2)
+                money = money + 100;
+
+            if (goverment == 3)
+                money = money + 200;
+
+            if(goverment == 4)
+                money = money + 400;
+
+            if(goverment == 5)
+                money = money + 500;
+
+            label5.Text = "Деньги: " + money;
+        }//если прокачиваешь правительство
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip gover = new ToolTip();
+            gover.SetToolTip(button1, "Правопарядок 0: Абсолютная анархия" +
+                "\nПравопарядок 1: Зачатки законов" +
+                "\nПравопарядок 2: Люди не бьют полицейских платиковыми стаканчиками"+
+                "\nПравопарядок 3: Воровство отныне вне закона"+
+                "\nПравопарядок 4: Коррупция? Нет, не слышали"+
+                "\nПравопарядок 5: А не слишком ли строго?");
+
+            ToolTip buy = new ToolTip();
+            buy.SetToolTip(button2, "Торговля 0: Что ты дашь за эта шкура?" +
+                "\nТорговля 1: Красивая бусинка, за неё возьму твой топр" +
+                "\nТорговля 2: Эти кристаллики блестят" +
+                "\nТорговля 3: Куплю тебя со всеми потрахами" +
+                "\nТорговля 4: Деньги наше всё" +
+                "\nТорговля 5: Деньги есть деньги");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (goverment == 5)
+                button1.Enabled = false;
+        }
+    }
+}
